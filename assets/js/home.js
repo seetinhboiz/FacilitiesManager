a3517067b6c33a10f7e210d768014b31f9dc5d58
nghianek123 <2051052089nghia@ou.edu.vn>
2023-08-23 16:38:35 +0700

    push

---
+++ assets/js/home.js
@@ -0,0 +1,23 @@
+const $ = document.querySelector.bind(document)
+const $$ = document.querySelectorAll.bind(document)
+
+const revealItems = $$('.reveal')
+window.addEventListener('scroll', reveal)
+
+//reveal
+function reveal(){
+    revealItems.forEach((item) => {
+        const windowHeight = window.innerHeight
+        const revealTop = item.getBoundingClientRect().top
+
+        // scrollY - offsetTop < innerHeight
+        
+        if (revealTop < windowHeight - 100) {
+            item.classList.add('active')
+        } 
+        //reveal lại khi kéo lên xong lại kéo xuống =))
+        else {
+            item.classList.remove('active')
+        }
+    })
+}
